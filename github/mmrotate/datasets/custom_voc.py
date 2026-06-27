00d1c0a6ebdaa9e2289a34d1a081e263362da87e
wrkzzzz1 <13671646623@163.com>
2026-06-27 23:35:30 +0800

    删除文件夹

--- github/mmrotate/datasets/custom_voc.py
+++
@@ -1,9 +0,0 @@
-from mmrotate.datasets.builder import ROTATED_DATASETS
-from mmrotate.datasets import VOCDataset
-
-@ROTATED_DATASETS.register_module()
-class CustomVOCDataset(VOCDataset):
-    def get_img_info(self, idx):
-        img_id = self.data_infos[idx]['id']
-        filename = f'{img_id}.bmp'  # bmp后缀
-        return dict(filename=filename)
--- github/mmrotate/models/necks/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .re_fpn import ReFPN
-from .SGM  import SGM
-from .CAMM  import CAMM
-from .GNN  import StructGNN
-__all__ = ['ReFPN','SGM','CAMM','StructGNN']
